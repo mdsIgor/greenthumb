927370fd7e1ee0e79f26d6409c7004eda5a9eb21
mdsigor <igor_melo_silva@hotmail.com>
2020-06-09 16:34:35 -0300

    issues on background-image. Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,27 +2,20 @@ import React from "react"
 import Button from "../../components/Button"
 import Container from "../../components/Container"
 import Logo from "../../components/Logo"
-import hero from "../../../static/images/hero.png"
 import "./Home.styl"
 
 const Home = () => {
   return (
     <Container>
       <main className="home">
-        <section className="home__heading">
-          <div className="logo-wrapper">
-            <Logo />
-          </div>
-          <div className="heading-wrapper">
-            <h1 className="home__heading__title">
-              Find your next green friend
-            </h1>
-            <Button text="start quiz" rightArrow dark />
-          </div>
-        </section>
-        <section className="home__hero">
-          <img src={hero}></img>
-        </section>
+        <div className="content-wrapper">
+          <Logo />
+          <h1>Find your next green friend</h1>
+          <Button dark rightArrow text="start quiz" />
+        </div>
+        <div className="img-wrapper">
+          <div className="bg-image"></div>
+        </div>
       </main>
     </Container>
   )
